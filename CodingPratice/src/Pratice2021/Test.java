334cf727b6e2008e0a33268d0a658d1d458140e7
Robin_Dev <76549540+robin-31@users.noreply.github.com>
2021-06-28 15:58:42 +0530

    Update Test.java
    just checking push  command

--- CodingPratice/src/Pratice2021/Test.java
+++ CodingPratice/src/Pratice2021/Test.java
@@ -5,6 +5,8 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("eclipse is working fine");
+		
+		System.out.println("push command is workin fine");
 	}
 
 }
